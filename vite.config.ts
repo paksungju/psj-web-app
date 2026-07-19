8e524085eca75aedd8432267eb3e841573d08811
parksungju <paksungju@naver.com>
2026-07-19 16:15:49 +0900

    설정값

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       proxy: {
-        '/api': proxyOpts,
+        '/api': { ...proxyOpts, ws: true },
         '/ask-stream': { ...proxyOpts, ws: true },
         '/ask-claude': proxyOpts,
         '/ask-gemini': proxyOpts,
